52a28d1f7bdeaf43b0be9d71325dd613613b64a6
Alexander Grein <alexander.grein@gmail.com>
2022-09-08 16:15:06 +0000

    Add new file

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Mail',
+    'description' => 'Newsletter System for TYPO3',
+    'category' => 'plugin',
+    'version' => '0.0.1',
+    'state' => 'alpha',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
