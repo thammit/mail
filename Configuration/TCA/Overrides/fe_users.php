73f15247e08b310f197a8eead0449d57a9351b84
Alexander Grein <alexander.grein@gmail.com>
2022-10-12 22:54:41 +0200

    Refactoring

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -10,18 +10,18 @@ $feUsersCols = [
             'type' => 'check'
         ]
     ],
-    'categories' => [
-        'label' => 'LLL:EXT:mail/Resources/Private/Language/locallang_tca.xlf:module_sys_dmail_group.category',
-        'exclude' => true,
-        'config' => [
-            'type' => 'category'
-        ]
-    ],
     'accepts_html' => [
         'label' => 'LLL:EXT:mail/Resources/Private/Language/locallang_tca.xlf:module_sys_dmail_group.htmlemail',
         'exclude' => '1',
         'config' => [
             'type' => 'check'
+        ]
+    ],
+    'categories' => [
+        'label' => 'LLL:EXT:mail/Resources/Private/Language/locallang_tca.xlf:module_sys_dmail_group.category',
+        'exclude' => true,
+        'config' => [
+            'type' => 'category'
         ]
     ],
 ];
