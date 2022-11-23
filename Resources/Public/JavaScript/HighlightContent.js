a3d308f25d8956731bea3a8c0d47c7a63685a2ec
Alexander Grein <alexander.grein@gmail.com>
2022-11-23 10:01:22 +0100

    Improve category wizard

--- Resources/Public/JavaScript/HighlightContent.js
+++ Resources/Public/JavaScript/HighlightContent.js
@@ -1,4 +1,5 @@
 define([], function() {
+    const mailWizardPreview = document.getElementById('mail-wizard-preview');
     document.querySelectorAll('[data-content-id]').forEach((row) => {
         row.addEventListener('mouseover', (event) => {
             const row = event.target.closest('[data-content-id]');
@@ -7,6 +8,7 @@ define([], function() {
             const originContainerBackgroundColor = contentElement.parentElement.style.backgroundColor;
             row.dataset.originBackgroundColor = originContainerBackgroundColor;
             contentElement.parentElement.style.backgroundColor = '#ea7676';
+            mailWizardPreview.scrollTop = contentElement.offsetTop - 20;
         });
         row.addEventListener('mouseout', (event) => {
             const row = event.target.closest('[data-content-id]');
