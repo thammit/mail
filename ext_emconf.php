dc6a8b9539d41e1ecfe8f34d4ea7de4dad409ae4
Alexander Grein <alexander.grein@gmail.com>
2024-06-29 08:43:28 +0000

    Update 2 files
    - /ext_emconf.php
    - /composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,21 +7,21 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Grein',
     'author_email' => 'alexander.grein@gmail.com',
     'author_company' => 'MEDIA::ESSENZ',
-    'version' => '2.6.4',
+    'version' => '3.0.0',
     'state' => 'stable',
     'constraints' => [
         'depends' => [
             'php' => '8.0.0-8.3.99',
-            'typo3' => '11.5.0-12.4.99',
-            'redirects' => '11.5.0-12.4.99',
-            'scheduler' => '11.5.0-12.4.99',
-            'fluid_styled_content' => '11.5.0-12.4.99',
-            'jumpurl' => '8.0.0-8.99.99',
-            'tt_address' => '7.0.0-8.99.99',
+            'typo3' => '11.5.0-13.4.99',
+            'redirects' => '11.5.0-13.4.99',
+            'scheduler' => '11.5.0-13.4.99',
+            'fluid_styled_content' => '11.5.0-13.4.99',
         ],
         'conflicts' => [
         ],
         'suggests' => [
+            'jumpurl' => '8.0.0-8.99.99',
+            'tt_address' => '7.0.0-9.99.99',
         ],
     ],
     'autoload' => [
